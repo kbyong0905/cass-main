8fdad198f95693b0041966236a26a1f68711cd4f
kbyong0905 <kongby-wm20@student.tarc.edu.my>
2023-01-02 16:32:12 +0800

    hi

--- EmpApp.py
+++ EmpApp.py
@@ -77,8 +77,8 @@ def AddEmp():
             cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
             db_conn.commit()
 
-        except Exception as e:
-            return str(e)
+    except Exception as e:
+        return str(e)
 
     finally:
         cursor.close()
