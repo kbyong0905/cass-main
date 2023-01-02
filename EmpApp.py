69cd6c9de9fcaa80de76fa7b2d6a7209aadee497
kbyong0905 <108616995+kbyong0905@users.noreply.github.com>
2023-01-03 03:22:28 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -200,8 +200,6 @@ def EditStaff():
             db_conn.commit()
             cursor.close()
               
-        finally:
-            cursor.close()
 
     #else got image upload, run image file query
     else:
@@ -230,31 +228,10 @@ def EditStaff():
             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
             s3_location = (bucket_location['LocationConstraint'])
             
-        finally:
-            cursor.close()
+        except Exception as e:
+            return str(e)
 
-    emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-    s3 = boto3.resource('s3')
 
-    try:
-        print("Data inserted in MySQL RDS... uploading image to S3...")
-        bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-        s3_location = (bucket_location['LocationConstraint'])
-        if s3_location is None:
-            s3_location = ''
-        else:
-            s3_location = '-' + s3_location
-
-        object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-        s3_location,
-        custombucket,
-        emp_image_file_name_in_s3)
-
-    except Exception as e:
-        return str(e)
-
-    print(object_url)
-    print(emp_id)
     return render_template('GetEmpOutput.html', result=resultdata, image_url=object_url)
 
 
