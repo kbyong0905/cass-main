28a375575a892f2594fe5ed983a0dd5938b16f43
kbyong0905 <108616995+kbyong0905@users.noreply.github.com>
2023-01-02 23:38:31 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -128,34 +128,34 @@ def AddEmp():
 def getEmp():
      emp_id = request.form['emp_id']
             
-        try:
-            db_conn.ping()
-            cursor = db_conn.cursor()          
-            insert_sql = "SELECT * FROM employee WHERE emp_id = %(emp_id)s"
-            cursor.execute(select_stmt, { 'emp_id': int(emp_id) })
-            db_conn.commit()
-            cursor.close()
+    try:
+        db_conn.ping()
+        cursor = db_conn.cursor()
+        insert_sql = "SELECT * FROM employee WHERE emp_id = %(emp_id)s"
+        cursor.execute(select_stmt, { 'emp_id': int(emp_id) })
+        db_conn.commit()
+        cursor.close()
 
-        except:
-            db_conn.ping()
-            cursor = db_conn.cursor()          
-            insert_sql = "SELECT * FROM employee WHERE emp_id = %(emp_id)s"
-            cursor.execute(select_stmt, { 'emp_id': int(emp_id) })
-            db_conn.commit()
-            cursor.close()
+    except:
+        db_conn.ping()
+        cursor = db_conn.cursor()          
+        insert_sql = "SELECT * FROM employee WHERE emp_id = %(emp_id)s"
+        cursor.execute(select_stmt, { 'emp_id': int(emp_id) })
+        db_conn.commit()
+        cursor.close()
 
-        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-        s3 = boto3.resource('s3')
+    emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+    s3 = boto3.resource('s3')
+    
+    try:
+        print("Data inserted in MySQL RDS... uploading image to S3...")
+        bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+        s3_location = (bucket_location['LocationConstraint'])
 
-        try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
-
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
+        if s3_location is None:
+            s3_location = ''
+        else:
+            s3_location = '-' + s3_location
 
             object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
                 s3_location,
